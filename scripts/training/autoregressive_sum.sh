17494b8b3693f254717a5d0d2666b70bd34aaba1
lvassertail <58955355+lvassertail@users.noreply.github.com>
2021-01-03 17:00:58 +0200

    scripts

--- scripts/training/autoregressive_sum.sh
+++ scripts/training/autoregressive_sum.sh
@@ -8,5 +8,5 @@
 --share-decoder-input-output-embed --share-all-embeddings --task summarisation --criterion label_smoothed_cross_entropy --arch transformer_summarisation \
 --optimizer adam --adam-betas "(0.9,0.98)" --clip-norm 0.0 --lr 0.0002 --lr-scheduler inverse_sqrt --min-lr 1e-09 \
 --warmup-init-lr 1e-07 --label-smoothing 0.1 --dropout 0.3 --weight-decay 0.0001 --log-format simple --log-interval 100 --fp16 \
---max-tokens 25000 \
+--max-tokens 35000 \
 --max-update 50000 --warmup-updates 4000
